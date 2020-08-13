91a3b33ee40016074c0abf8b0be455a5659907c4
Suraj S Jain <surajsjain@hotmail.com>
2020-08-13 17:37:31 +0530

    Initial Commit

--- PaymentGateway/settings.py
+++ PaymentGateway/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'ubei=po6jyu&(s$83gas1@5yl@&2qoio($!)+hbr&n&^xqk8$('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainsite.apps.MainsiteConfig'
+    'usermgmt.apps.UsermgmtConfig'
 ]
 
 MIDDLEWARE = [
